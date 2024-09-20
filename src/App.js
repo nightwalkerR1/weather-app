25aeb59289f7fc4a525dbf55024c562d897469b6
nightwalkerR1 <ayushvishwakrma8@gmail.com>
2024-09-20 15:33:49 +0530

    weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="weather-main" >
+      <h1>weather app</h1>
+      <div>
+        <div className="container">
+          <div className="row"></div>
+          <div className="col-md-4">col 1</div>
+          <div className="col-md-8"> col 2</div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
